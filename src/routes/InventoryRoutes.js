24491b1ba6c067e8749844e0b67ce787d306d4b7
reshkarthik <reshmakarthik@Reshmas-MacBook-Pro-2.local>
2020-11-22 15:43:44 -0800

    includes popular recipes, get all ingredients, delete from inventory, send email for click on google

--- src/routes/InventoryRoutes.js
+++ src/routes/InventoryRoutes.js
@@ -11,74 +11,216 @@ const requireAuth = require('../middlewares/requireAuth');
 const mongoUri =
   'mongodb+srv://cse110:gary@cwc.l4ds3.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
-MongoClient.connect(mongoUri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
 const router = express.Router();
+
+/**
+ * view inventory
+ */
+router.get('/inventory', requireAuth, async (req, res) => {
+  try {
+    return res.send(req.user.inventory);
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error inventory cannot be viewed' });
+  }
+});
 
 /**
  * update inventory - add, delete
  */
 router.post('/inventory', requireAuth, async (req, res) => {
-  const { ingredients } = req.body;
-  req.user.inventory = ingredients;
-  req.user.save();
-  return res.json({ message: 'Success inventory updated' });
+  try {
+    const { ingredients } = req.body;
+    req.user.inventory = ingredients;
+    req.user.save();
+    return res.json({ message: 'Success inventory updated' });
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error inventory cannot be updated' });
+  }
+});
+
+/**
+ * view saved recipes
+ */
+router.get('/savedRecipes', requireAuth, async (req, res) => {
+  try {
+    return res.send(req.user.recipe);
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error inventory cannot be viewed' });
+  }
 });
 
 /**
  * update saved recipes - add, delete
  */
 router.post('/recipe', requireAuth, async (req, res) => {
-  const { recipes } = req.body;
-  req.user.recipe = recipes;
-  req.user.save();
-  return res.json({ message: 'Success recipe added' });
+  try {
+    const { recipes } = req.body;
+    req.user.recipe = recipes;
+    req.user.save();
+    return res.json({ message: 'Success recipe added' });
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error saved recipe cannot be updated' });
+  }
 });
 
 /**
  * update preferences - change from true to false or vice versa
  */
 router.post('/preferences', requireAuth, async (req, res) => {
-  const { preference } = req.body;
-  req.user.preferences = preference;
-  console.log(req.user.preferences.vegan);
-  req.user.save();
-
-  return res.json({ message: 'Success preference added' });
+  try {
+    const { preference } = req.body;
+    req.user.preferences = preference;
+    req.user.save();
+    return res.json({ message: 'Success preference added' });
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error preferences cannot be updated' });
+  }
 });
 
-router.post('/addRecentRecipe', requireAuth, async (req, res) => {
+/**
+ * add recipe to user's recent recipe list
+ */
+router.post('/recentRecipe', requireAuth, async (req, res) => {
   const { recipe } = req.body;
+  if (req.user.recentRecipes.includes(recipe)) {
+    const index = req.user.recentRecipes.indexOf(recipe);
+    req.user.recentRecipes.splice(index, 1);
+  }
   if (req.user.recentRecipes.length > 20) {
     req.user.recentRecipes.splice(19, 1);
   }
-  req.user.recentRecipes.push(recipe);
+  req.user.recentRecipes.splice(0, 0, recipe);
   req.user.save();
 
-  return res.json({ message: 'Success preference added' });
+  return res.json({ message: 'Success recent recipe added' });
 });
 
+/**
+ * view user's recent recipe list
+ */
 router.get('/recentRecipe', requireAuth, async (req, res) => {
-  res.send(req.user.recentRecipes);
+  try {
+    res.send(req.user.recentRecipes);
+  } catch (e) {
+    console.log(e);
+    return res.json({ message: 'Error recent recipes cannot be viewed' });
+  }
 });
 
-router.get('/recentRecipe', requireAuth, async (req, res) => {
-  res.send(req.user.recentRecipes);
+/**
+ * view most popular recipes list
+ */
+router.get('/popularRecipes', requireAuth, async (req, res) => {
+  const client = new MongoClient(mongoUri);
+  try {
+    await client.connect();
+
+    const cursor = await client
+      .db('<dbname>')
+      .collection('recipes')
+      .find()
+      .sort({
+        aggregateLikes: -1,
+      })
+      .limit(30);
+    const result = await cursor.toArray();
+    return res.send(result);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 });
 
-// router.get('/allIngredients', requireAuth, async (req, res) => {
-//   MongoClient.connect(mongoUri, (err, db) => {
-//     if (err) throw err;
-//     db.db('mydb')
+/**
+ * view all ingredients from database
+ */
+router.get('/allIngredients', requireAuth, async (req, res) => {
+  const client = new MongoClient(mongoUri);
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    const cursor = await client.db('<dbname>').collection('ingredients').find();
+    const result = await cursor.toArray();
+    return res.send(result);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+});
+
+// // Returns the actual ingredient given the ingredient's object id.
+// async function getIngredient(objectID) {
+//   const client = new MongoClient(mongoUri);
+//   try {
+//     await client.connect();
+
+//     const result = await client
+//       .db('<dbname>')
 //       .collection('ingredients')
-//       .find()
-//       .toArray((err, data) => {
-//         if (data != null) response.send(data);
-//         db.close();
-//       });
+//       .findOne((elem) => elem === objectID);
+//     return result;
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
+// // Returns a score of how well a user's inventory matches with a recipe.
+// function matchIngredients(recipeIngredients, inventory) {
+//   let i = 0;
+//   let count = 0;
+//   for (i = 0; i < inventory.length; i += 1) {
+//     if (recipeIngredients.includes(getIngredient(inventory[i]))) {
+//       count += 1;
+//     }
+//   }
+//   return count / inventory.length;
+// }
+
+// router.get('/possiblerecipes', requireAuth, async (req, res) => {
+//   // Get the ingredients from the object id
+//   // take the ingredients from the user's inventory and match it with a given recipe which will return a score
+//   // Add the scores to a list once the list is filled. Sort it and return the top 20.
+//   console.log(req.user.inventory);
+//   const returnRecipes = {};
+//   const client = new MongoClient(mongoUri);
+
+//   const cursor = await client
+//     .db('<dbname>')
+//     .collection('recipes')
+//     .find()
+//     .limit(30);
+//   const temp = await cursor.toArray();
+//   temp.array.forEach((element) => {
+//     const score = matchIngredients(
+//       element.extendedIngredients,
+//       req.user.inventory,
+//     );
+//     if (score in returnRecipes) {
+//       returnRecipes[score].push(element);
+//     } else {
+//       returnRecipes[score] = [element];
+//     }
 //   });
+//   const result = [];
+//   const scores = Object.keys(returnRecipes).array.sort();
+//   let num = 0;
+//   while (result.length < 20) {
+//     let i = 0;
+//     for (i = 0; i < returnRecipes[scores[num]].length; i += 1) {
+//       result.push(returnRecipes[scores[num]][i]);
+//     }
+//     num += 1;
+//   }
+//   res.send(req.user.inventory);
 // });
 
 module.exports = router;
